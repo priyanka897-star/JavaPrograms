35ff797d311a8dc669c7d1de45b9bc23ae6c2e24
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-09-29 01:06:34 +0530

    Added:Code to check given character is Vowel or Consonant

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -9,7 +9,7 @@ public class VowelConsonant {
 		if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
 			System.out.println(alphabet + " is vowel");
 		} else {
-			System.out.println(alphabet + " is consonant");
+			System.out.println(alphabet + " is consonant" );
 		}
 	}
 
