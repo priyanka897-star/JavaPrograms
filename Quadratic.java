e0efb896c534d034d4bbd6a73abcb6c7be3c2d9d
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-09-29 01:01:47 +0530

    Modified:Content of the code ,Indentation Correction

--- Quadratic.java
+++ Quadratic.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Quadratic {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a,b,c Values  ");// a*x*x + b*x + c.
+		System.out.println("Enter a,b,c  Values  ");// a*x*x + b*x + c.
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
 		int c = scanner.nextInt();
