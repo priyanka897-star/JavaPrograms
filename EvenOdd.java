586d38c3d1f0849de081131fd43df22385bbc32e
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-09-29 00:48:14 +0530

    Added: Code to check Number is even or odd

--- EvenOdd.java
+++ EvenOdd.java
@@ -8,9 +8,9 @@ public class EvenOdd {
 		System.out.println("Enter The Number: ");
 		int number = scanner.nextInt();
 		if (number % 2 == 0) {
-			System.out.println(" Number is Even");
+			System.out.println(" Number is Even ");
 		} else {
-			System.out.println("Number is Odd");
+			System.out.println("Number is Odd ");
 		}
 	}
 
